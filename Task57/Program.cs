7de2e62a350976c6cc4b437f63c591395d40ee89
Dinis <Dinis_abd@example.com>
2022-08-29 22:39:14 +0300

    task57,59

---
+++ Task57/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+
+int[,] CreateIntMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки - row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы- col
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            Console.Write(j < matrix.GetLength(1) - 1 ?
+            $"{matrix[i, j],4}," : $"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void Dictionary(int[,] matrix)
+{
+    int[] array = new int[matrix.Length];
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[k] = matrix[i, j];
+            k++;
+        }
+    }
+
+    Array.Sort(array);
+
+    int count = 1;
+    int value = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] != value)
+        {
+            Console.WriteLine($"Значение {value} встречается {count} раз");
+            value = array[i];
+            count = 1;
+        }
+        else count++;
+    }
+    Console.WriteLine($"Значение {value} встречается {count} раз");
+}
+
+int[,] mtrx = CreateIntMatrix(4, 3, 1, 9);
+PrintMatrix(mtrx);
+
+Console.WriteLine();
+
+Dictionary(mtrx);
